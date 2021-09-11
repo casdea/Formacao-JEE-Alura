42f1ee26de719b31af596aad5604c3d69bfbedf5
casdea <casdea@gmail.com>
2021-09-11 08:16:24 -0300

    Criando e Configurando o Projeto

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,11 +1,16 @@
 package br.com.casadocodigo.loja.beans;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
 import br.com.casadocodigo.loja.dao.LivroDao;
+import br.com.casadocodigo.loja.model.Autor;
 import br.com.casadocodigo.loja.model.Livro;
 
 @Named
@@ -13,14 +18,17 @@ import br.com.casadocodigo.loja.model.Livro;
 public class AdminLivrosBean {
 
 	private Livro livro = new Livro();
-	
+
+	private List<Long> autoresId = new ArrayList<Long>();
+
 	@Inject
 	private LivroDao livroDao;
 
 	@Transactional
 	public void salvar() {
+		System.out.println(autoresId);
 		livroDao.salvar(livro);
-		System.out.println("Livro Salvo "+livro.toString());
+		System.out.println("Livro Salvo " + livro.toString());
 	}
 
 	public Livro getLivro() {
@@ -29,6 +37,18 @@ public class AdminLivrosBean {
 
 	public void setLivro(Livro livro) {
 		this.livro = livro;
+	}
+
+	public List<Autor> getAutores() {
+		return Arrays.asList(new Autor(1L, "CARLOS"), new Autor(2L, "MANOEL"));
+	}
+
+	public List<Long> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Long> autoresId) {
+		this.autoresId = autoresId;
 	}
 
 }
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Livro.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Livro.java
@@ -10,7 +10,7 @@ import javax.persistence.Lob;
 
 @Entity
 public class Livro {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
