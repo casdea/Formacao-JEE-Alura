564e47da3a62062d835ce86d2a9bdde2ed8aeda9
casdea <casdea@gmail.com>
2021-09-12 21:34:48 -0300

    Relacionando livro com autores

--- testes-com-mock/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ testes-com-mock/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -1,36 +1,53 @@
 package br.com.alura.leilao.service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
 
 class GeradorDePagamentoTest {
 
 	@Mock
-	private PagamentoDao pagamentos;
+	private PagamentoDao mockPagamentoDao;
 
 	private GeradorDePagamento geradorDePagamento;
+	
+	@Captor
+	private ArgumentCaptor<Pagamento> captorPagamento;
 
 	@BeforeEach
 	public void beforeEach() {
 		MockitoAnnotations.initMocks(this);
-		this.geradorDePagamento = new GeradorDePagamento(pagamentos);
+		this.geradorDePagamento = new GeradorDePagamento(mockPagamentoDao);
 	}
 	
 	@Test
 	void deveriaCriarPagamentoParaVencedorDoLeilao() {
 		Leilao leilao = obterLeilao();
 		Lance lanceVencedor = leilao.getLanceVencedor();
+		 
+		this.geradorDePagamento.gerarPagamento(lanceVencedor);
+		Mockito.verify(mockPagamentoDao).salvar(captorPagamento.capture());
+		
+		Pagamento pagamento = captorPagamento.getValue();
+		Assert.assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+		Assert.assertEquals(lanceVencedor.getValor(), pagamento.getValor());
+		Assert.assertFalse(pagamento.getPago());
+		Assert.assertEquals(lanceVencedor.getUsuario(), pagamento.getUsuario());
+		Assert.assertEquals(leilao, pagamento.getLeilao());
 	}
 
 	private Leilao obterLeilao() {
